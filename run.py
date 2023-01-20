3b54318af85f3f9c0ed09196f2016dabd60bec8d
Mark-O-S <mark96basilan@yahoo.com>
2023-01-20 23:31:49 +0000

    Add instructions on how to play and put it inside the main function

--- run.py
+++ run.py
@@ -1,10 +1,14 @@
 
 
 def main():
-    player_name = input("Welcome to Bulls and Calves!\nPlease enter your name: ")
-    print(f"Hello {player_name}, please read the instructions to know how to play the game!")
+    print("Welcome to Bulls and Calves!\n")
+    print("***How To Play***\nGuess the right number!\nIf a number is one of the correct digits and is in the correct position, it's a bull.\nIf a number is one of the correct digits but is in the incorrect position, it's a calf.\n")
+    player_name = input("Please enter your name: ")
+    print(f"Hello {player_name}, please make sure to read the instructions to know how to play!")
+
 
     user_input = input("Enter your guess: ")
     print(user_input)
+
 
 main()

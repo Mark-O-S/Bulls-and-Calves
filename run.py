dd469074d129b92dd36ff468500026bba043dc30
Mark-O-S <mark96basilan@yahoo.com>
2023-01-22 21:42:09 +0000

    Edit restart option so that if the user chooses yes he would be redirected back to choosing game difficulty

--- run.py
+++ run.py
@@ -66,10 +66,11 @@ def check_for_bull_or_calf(list_user_input, game_code):
 
     return False
 
-yeslist = ["yes", "y", "ya", "yeah", "yep"]
 """
 Allows the program to accept different types of 'yes' responses.
 """
+yeslist = ["yes", "y", "ya", "yeah", "yep"]
+
 
 def main():
     """
@@ -85,55 +86,56 @@ def main():
     print("Once you guess all Bull's you win the game!\n")
     player_name = input("Please enter your name: ")
     print(f"Hello {player_name}! Choose your difficulty below - How many digits would you like to guess?\n")
-  
-    
-    # Validate difficulty set by user - Keep prompting user until they provide one of the expected difficulties.
-    valid_difficulty = False
-    while not valid_difficulty:
-        difficulty_level = input("Options are 2, 3 or 4\n")
-        valid_difficulty = validate_game_difficulty(difficulty_level)
-
-        # Check if the game difficulty provided is not valid and if not send a message to choose one of the options
-        if not valid_difficulty:
-            print("You provided an invalid game difficulty - please choose one of the options below")
-        
-        if valid_difficulty:
-            difficulty_level = int(difficulty_level)
-            print(f"You have chosen difficulty level: {difficulty_level} digits.\nGood luck!")
 
     
-    game_code = generate_game_code(difficulty_level)
-    """
-    This will generate a random code used by the game
-    """
-    # TO DO - Remove print(game_code)
-    print(game_code)  
+    while True:
 
-    # Create while loop to keep the user guessing until they guess the game code correctly
-    guessing_game_code = False
-    while not guessing_game_code:
-        user_input = input("Enter your guess: ")
-        list_user_input = list(user_input)
-        valid_user_code_guess = validate_user_code_guess(list_user_input, difficulty_level)
+        # Validate difficulty set by user - Keep prompting user until they provide one of the expected difficulties.
+        valid_difficulty = False
+        while not valid_difficulty:
+            difficulty_level = input("Options are 2, 3 or 4\n")
+            valid_difficulty = validate_game_difficulty(difficulty_level)
 
-        # If the user_code_guess is not valid  
-        if not valid_user_code_guess:
-            print(f"Invalid guess, please enter a {difficulty_level} digit unique number")
+            # Check if the game difficulty provided is not valid and if not send a message to choose one of the options
+            if not valid_difficulty:
+                print("You provided an invalid game difficulty - please choose one of the options below:")
         
-        # if the user code guess is valid
+            if valid_difficulty:
+                difficulty_level = int(difficulty_level)
+                print(f"You have chosen difficulty level: {difficulty_level} digits.\nGood luck!")
+    
+        game_code = generate_game_code(difficulty_level)
+        """
+        This will generate a random code used by the game
+        """
+        # Note to self - Remove print(game_code)
+        print(game_code)  
+
+        # Create while loop to keep the user guessing until they guess the game code correctly
+        guessing_game_code = False
+        while not guessing_game_code:
+            user_input = input("Enter your guess: ")
+            list_user_input = list(user_input)
+            valid_user_code_guess = validate_user_code_guess(list_user_input, difficulty_level)
+
+            # If the user_code_guess is not valid  
+            if not valid_user_code_guess:
+                print(f"Invalid guess, please enter a {difficulty_level} digit unique number")
+        
+            # if the user code guess is valid
+            else:
+                check_bulls_calves = check_for_bull_or_calf(list_user_input, game_code)
+                if check_bulls_calves:
+                    guessing_game_code = True
+        print(f"Congratulations {player_name}! You guessed all the numbers correctly!!!")
+
+        # Note to self - create an option for user to play again or not
+        restart = input("Would you like to play again?\nIf so, type 'Yes' if not, type any other word: ").lower()
+        if restart in yeslist:
+            guessing_game_code = True
         else:
-            check_bulls_calves = check_for_bull_or_calf(list_user_input, game_code)
-            if check_bulls_calves:
-                guessing_game_code = True
-    print(f"Congratulations {player_name}! You guessed all the numbers correctly!!!")
-
-    # Note to self - create an option for user to play again or not
-    restart = input("Would you like to play again?\nIf so, type 'Yes' if not, type any other word: ").lower()
-    if restart in yeslist:
-        main()
-    else:
-        print(f"Thank you {player_name}, for playing!")
-        exit()
+            print(f"Thank you for playing {player_name}!")
+            exit()
 
 if __name__ == '__main__':
     main()

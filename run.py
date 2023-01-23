b4e5970527c943fd079297b82887c029d0607200
Mark-O-S <mark96basilan@yahoo.com>
2023-01-23 19:12:55 +0000

    Move the const 'YESLIST and bring it to the top and capitalize it.

--- run.py
+++ run.py
@@ -3,10 +3,12 @@ import random
 """NUMBERS const will be used to help generate the game code."""
 NUMBERS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
+# Allows the program to accept different types of 'yes' responses.
+YESLIST = ["yes", "y", "ya", "ye", "yeah", "yep"] 
 
 def generate_game_code(code_length):
     """
-    This function is used to generate the code for the game.
+    This is used to generate the code for the game.
     random.sample is used to generate unique random numbers that 
     the user will try to guess to win the game.
     """ 
@@ -15,7 +17,7 @@ def generate_game_code(code_length):
 
 def validate_game_difficulty(game_difficulty):
     """
-    This function will validate the game in any chosen difficulty.
+    Validates the game in any chosen difficulty.
     """
     if game_difficulty in ["2", "3", "4"]:
         return True
@@ -52,9 +54,9 @@ def check_for_bull_or_calf(list_user_input, game_code):
     calves = 0
     
     # Loop through the indices of the user input list and ensure user inputs are converted as integers.
-    for index in range(len(list_user_input)):
-        if int(list_user_input[index]) in game_code:
-            if int(list_user_input[index]) == game_code[index]:
+    for index, item in enumerate(list_user_input):
+        if int(item) in game_code:
+            if int(item) == game_code[index]:
                 bulls += 1
             else:
                 calves += 1
@@ -65,11 +67,6 @@ def check_for_bull_or_calf(list_user_input, game_code):
 
     return False
 
-"""
-Allows the program to accept different types of 'yes' responses.
-"""
-yeslist = ["yes", "y", "ya", "ye", "yeah", "yep"]
-
 
 def main():
     """
@@ -78,7 +75,7 @@ def main():
     print("-----------------------------")
     print("WELCOME TO BULLS AND CALVES!")
     print("-----------------------------")
-    print("***HOW TO PLAY***\nGuess the right code!\nNote that you can only guess unique numbers from 0 - 9.")
+    print("***HOW TO PLAY***\nGuess the right code!\nNote that you can only guess unique numbers from 0 - 9.") # noqa
     print("Numbers cannot be repeated each time you enter a guess, for example: entering '111' or '121' will be invalid.")
     print("If a number is one of the correct digits and is in the correct position, it's a bull.")
     print("If a number is one of the correct digits but is in the incorrect position, it's a calf.")
@@ -108,12 +105,10 @@ def main():
             if valid_difficulty:
                 difficulty_level = int(difficulty_level)
                 print(f"You have chosen difficulty level: {difficulty_level} digits.\nGood luck!")
-    
-        """
-        This will generate a random code used by the game
-        """
+        
+        # This will generate a random code used by the game      
         game_code = generate_game_code(difficulty_level)
- 
+        print(game_code)
 
         # Create while loop to keep the user guessing until they guess the game code correctly.
         guessing_game_code = False
@@ -136,10 +131,11 @@ def main():
                     guessing_game_code = True
         print(f"\nCongratulations {player_name}! You guessed all the numbers correctly!!!\n")
 
+        restart = input(
+            "Would you like to play again?\nIf so, type 'Yes'. If not, type any other word: "
+            ).lower()
 
-        restart = input("Would you like to play again?\nIf so, type 'Yes'. If not, type any other word: ").lower()
-        
-        if restart in yeslist:
+        if restart in YESLIST:
             guessing_game_code = True
         else:
             print(f"Thank you for playing {player_name}. You guessed correctly {player_wins} times!")

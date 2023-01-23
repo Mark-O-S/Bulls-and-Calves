c015a2d2e797df99cbc14f86b47070382142f644
Mark-O-S <mark96basilan@yahoo.com>
2023-01-23 00:53:30 +0000

    Add flowchart

--- run.py
+++ run.py
@@ -82,9 +82,10 @@ def main():
     print("Numbers cannot be repeated each time you enter a guess, for example: entering '111' or '121' will be invalid.")
     print("If a number is one of the correct digits and is in the correct position, it's a bull.")
     print("If a number is one of the correct digits but is in the incorrect position, it's a calf.")
+    print("The difficulty level is gauged by how many digits you are guessing.\n2 digits being the easiest and 4 digits being the hardest.")
     print("Once you guess all Bull's you win the game!\n")
     player_name = input("Please enter your name: ")
-    print(f"Hello {player_name}! Choose your difficulty below - How many digits would you like to guess?\n")
+    print(f"\nHello {player_name}! Please choose your level of difficulty below:\n")
 
     player_wins = 0
     
@@ -93,7 +94,7 @@ def main():
         # Validate difficulty set by user - Keep prompting user until they provide one of the expected difficulties.
         valid_difficulty = False
         while not valid_difficulty:
-            difficulty_level = input("Options are 2, 3 or 4\n")
+            difficulty_level = input("Would you like to guess 2, 3 or 4 digits?\n")
             valid_difficulty = validate_game_difficulty(difficulty_level)
 
             # Check if the game difficulty provided is not valid and if not send a message to choose one of the options.
